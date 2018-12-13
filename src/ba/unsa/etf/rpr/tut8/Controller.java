0ddb70ac575b14beff763fbfc5e038ef6f0c4ea3
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-12-13 14:36:26 +0100

    Dodan mailProzor

--- src/ba/unsa/etf/rpr/tut8/Controller.java
+++ src/ba/unsa/etf/rpr/tut8/Controller.java
@@ -6,13 +6,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class Controller {
 
@@ -113,6 +121,23 @@ public class Controller {
             } else { // if f is a directory
                 traverseFiles(substr, f, currentSearchNumber);
             }
+        }
+    }
+
+    public void listViewClick(MouseEvent mouseEvent) {
+        Parent root = null;
+        try {
+            Stage myStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("mailProzor.fxml"));
+            loader.load();
+            MailController mailController;
+            mailController = loader.getController();
+            myStage.setTitle("Slanje datoteke");
+            myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            myStage.setResizable(false);
+            myStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
